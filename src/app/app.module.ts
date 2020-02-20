129507eab92f3e91361f4559e710e5b801527909
erwan <erwanp.vallee@gmail.com>
2020-02-20 13:36:19 +0100

    maj ng9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { UserService } from './user.service';
-import { ReqInterceptor } from './http.interceptor';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { UserService } from "./user.service";
+import { ReqInterceptor } from "./http.interceptor";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [
     UserService,
     {
@@ -25,4 +19,4 @@ import { ReqInterceptor } from './http.interceptor';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -1,6 +1,8 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class ReqInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log(req);
